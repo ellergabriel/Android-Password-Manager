781730b3960296cd9f0fd291cca3a74960255743
ellergabriel <ellergabriel13@gmail.com>
2022-04-12 10:58:04 -0700

    Debug mode with username/password field pair added.

---
+++ app/src/main/java/com/example/knox/DebugActivity.java
@@ -0,0 +1,16 @@
+package com.example.knox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+
+
+    }
+}
--- app/src/main/java/com/example/knox/MainActivity.java
+++ app/src/main/java/com/example/knox/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.knox;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -34,4 +36,8 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    public void debugMode(View view){
+        Intent intent = new Intent(this, DebugActivity.class);
+        startActivity(intent);
+    }
 }
