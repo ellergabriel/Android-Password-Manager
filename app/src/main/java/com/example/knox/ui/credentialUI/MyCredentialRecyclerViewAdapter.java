f84e4b5109bb39d8c5d586f373e61251510db0a3
ellergabriel <ellergabriel13@gmail.com>
2022-05-11 19:35:36 -0700

    Edit/delete button functionality both buttons can now be clicked and activated. implementation coming soon

--- app/src/main/java/com/example/knox/ui/credentialUI/MyCredentialRecyclerViewAdapter.java
+++ app/src/main/java/com/example/knox/ui/credentialUI/MyCredentialRecyclerViewAdapter.java
@@ -4,7 +4,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.knox.systemComponents.Credentials;
@@ -41,7 +43,19 @@ public class MyCredentialRecyclerViewAdapter extends RecyclerView.Adapter<MyCred
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = allCreds.get(position);
         holder.mIdView.setText(allCreds.get(position).url);
-
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo: popup window to edit the login
+            }
+        });
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo: popup window to delete a credential
+            }
+        });
+        //holder.itemView.setOnClickListener(holder.);
         //holder.mContentView.setText(allCreds.get(position).uName);
 //        holder.mIdView.setText(mValues.get(position).id);
 //        holder.mContentView.setText(mValues.get(position).content);
@@ -56,10 +70,13 @@ public class MyCredentialRecyclerViewAdapter extends RecyclerView.Adapter<MyCred
         public final TextView mIdView;
         //public final TextView mContentView;
         public Credentials mItem;
-
+        public final Button edit;
+        public final Button delete;
         public ViewHolder(FragmentItemBinding binding) {
             super(binding.getRoot());
             mIdView = binding.itemNumber;
+            edit = binding.editButton;
+            delete = binding.deleteButton;
             //mContentView = binding.content;
         }
 
